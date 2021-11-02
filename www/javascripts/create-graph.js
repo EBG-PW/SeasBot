6ffc87d5a7860b52dd5bbeda707e7df77a0874c6
BolverBlitz <bolverblitz@ebg.pw>
2021-11-02 01:03:28 +0100

    Updated WEB

--- www/javascripts/create-graph.js
+++ www/javascripts/create-graph.js
@@ -5,25 +5,36 @@ function parseData(createGraph) {
 	Papa.parse("./data/trashC.csv", {
 		download: true,
 		complete: function(results) {
-			createGraph(results.data);
+			createGraph(results.data, "1");
+			createGraph(results.data, "2");
 		}
 	});
 }
 
-function createGraph(data) {
+function createGraph(data, id) {
 	var years = [];
-	var Done = ["Trees in (K)"];
-	var Build = ["Difference since last measuring"]
-
-	for (var i = 1; i < data.length; i++) {
-		years.push(data[i][0]);
-		Done.push(data[i][1]);
-		Build.push(data[i][2]);
+	let Done, Build
+	if(id === "1"){
+		Done = ["Trash removed in thousend pounds"];
+		Build = ["Difference since last measuring in thousend pounds"]
+		for (var i = 1; i < data.length; i++) {
+			years.push(data[i][0]);
+			Done.push(data[i][1]*1000);
+			Build.push(data[i][2]);
+		}
+	}else{
+		Done = ["Trash removed in KG"];
+		Build = ["Difference since last measuring in KG"]
+		for (var i = 1; i < data.length; i++) {
+			years.push(data[i][0]);
+			Done.push(((data[i][1]*1000)*0.45359237).toFixed(2));
+			Build.push(((data[i][2])*0.45359237).toFixed(2));
+		}
 	}
-
+	
 
 	var chart = c3.generate({
-		bindto: '#chart',
+		bindto: `#chart${id}`,
 	    data: {
 	        columns: [
 				Done,
@@ -38,7 +49,8 @@ function createGraph(data) {
 	            	multiline: false,
                 	culling: {
                     	max: 15
-                	}
+                	},
+					beginAtZero: true,
             	}
 	        }
 	    },
